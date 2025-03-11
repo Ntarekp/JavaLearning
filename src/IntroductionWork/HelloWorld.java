82cd0590e5b2924de75e11232b93aaf501fb4aaa
Ntarekp <kpntare@gmail.com>
2025-03-11 07:59:51 +0200

    Hello World program

--- src/IntroductionWork/HelloWorld.java
+++ src/IntroductionWork/HelloWorld.java
@@ -1,5 +1,10 @@
-public class Main {
+package IntroductionWork;
+
+public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
     }
+
+
 }
